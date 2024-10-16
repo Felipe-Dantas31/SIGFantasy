4e96dc4391d9273620ad552f7dc9f088e4140b9f
Felipe Iago Dantas <liodan31@gmail.com>
2024-10-16 16:16:15 -0300

    criação assinaturas cliente.h

---
+++ cliente.h
@@ -0,0 +1,7 @@
+char menu_principal(void);
+char modulo_cliente(void);
+char menu_cliente(void);
+void atualizar_cliente(void);
+void excluir_cliente(void);                     
+void pesquisar_cliente(void);
+void cadastrar_cliente(void);
