75f577910484c19e7a667ccf0da04537645a774d
Felipe Iago Dantas <liodan31@gmail.com>
2024-10-16 16:05:45 -0300

    Função de navegção do módulo cliente

---
+++ cliente.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char modulo_cliente(void) {
+    char opcao_c;
+    do {
+        opcao_c = menu_cliente();
+        switch(opcao_c) {
+            case '1': cadastrar_cliente();
+                      break;
+            case '2': atualizar_cliente();
+                      break;
+            case '3': excluir_cliente();
+                      break;
+            case '4': pesquisar_cliente();
+                      break;
+        }
+    } while(opcao_c != '0');
+    return 0;
+}
