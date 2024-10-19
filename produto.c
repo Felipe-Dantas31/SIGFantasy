f8ea11301a8d13798fac5d5310177a87feeeeb27
Francenildo <francenildo.costa.017@ufrn.edu.br>
2024-10-19 12:24:08 -0300

    add menu produto

--- produto.c
+++ produto.c
@@ -19,3 +19,25 @@ char modulo_produto(void) {
     } while(opcao_p != '0');
     return 0;
 }
+
+char menu_produto(void) {
+    system("clear||cls");
+    char op;
+    printf("\n");
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("@@@                            Sis-Fantasy                                  @@@\n");
+    printf("@@@                      Developed By NIL and Felipe                        @@@\n");
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("@@@                         * * * PRODUTO * * *                             @@@\n");
+    printf("@@@                         1 * CADASTRAR PRODUTO                           @@@\n");
+    printf("@@@                         2 * ATUALIZAR PRODUTO                           @@@\n");
+    printf("@@@                         3 * EXCLUIR PRODUTO                             @@@\n");
+    printf("@@@                         4 * PESQUISAR PRODUTO                           @@@\n");
+    printf("@@@                         0 * VOLTAR                                      @@@\n");
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("\n");
+    printf("Escolha uma opcao: ");
+    scanf(" %c", &op);
+    getchar(); 
+    return op;
+}
