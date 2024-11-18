233869505360ac64f9bddda9b68e34ea407079e2
Expedito <expedito.luiz@escolar.ifrn.edu.br>
2024-11-18 15:57:56 -0300

    Adição struct cliente

--- cliente.h
+++ cliente.h
@@ -3,4 +3,11 @@ char menu_cliente(void);
 void cadastrar_cliente(void);
 void atualizar_cliente(void);
 void pesquisar_cliente(void);
-void excluir_cliente(void);  
+void excluir_cliente(void);
+
+typedef struct cliente Cliente;
+
+struct cliente
+{
+    char cpf[15], nome[50], fone[19], email[50], endereco[100];
+};
